03279d7c211c7af72814636437c37742c95de5d8
Paul Janson <pauljanson002@gmail.com>
2021-09-23 19:41:19 +0530

    Added page navigator

---
+++ screens/providers.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text,View} from "react-native";
+
+const ProviderScreen = ()=>{
+    return(
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+            <Text>Providers</Text>
+        </View>
+    )
+}
+export default ProviderScreen
